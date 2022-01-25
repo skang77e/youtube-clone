255986f396129e50b2134616d8cb48e76778515f
Jon Kang <jk2060@georgetown.edu>
2022-01-24 22:10:27 -0500

    hide api key to .env

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,11 +1,8 @@
 import axios from "axios";
-const KEY = "AIzaSyAnmy06BgbFxr_rtsA1xayyRECO9eGwqv4";
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3/",
   params: {
-    part: "snippet",
-    maxResults: 5,
-    key: KEY,
+    key: process.env.REACT_APP_YOUTUBE_API_KEY,
   },
 });
